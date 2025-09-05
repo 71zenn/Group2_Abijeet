1af7fdb3ec71d6a97f8a92af71fd27b112db2f08
71zenn <abijeetshrestha573@gmail.com>
2025-09-05 14:10:38 +0545

    1st task of Programming Professionals

---
+++ week1/Abijeet/Hello.java
@@ -0,0 +1,50 @@
+public class Hello {
+    public static void main(String[] args){
+        //Question 1.
+        System.out.println("Hello World!");
+        System.out.println();
+
+        //Question 2.
+        System.out.println("This is my first program");
+        System.out.println("I am on module ST4003CEM");
+        System.out.println();
+
+
+        //Question 3. 
+        System.out.println("A \"quoted\" String is");
+        System.out.println("\'much\' better if you learn");
+        System.out.println("the rules of \"escape sequences.\"");
+        System.out.println("Also, \"\" represents an empty");
+        System.out.println("String. Don't forget: use \\\"");
+        System.out.println("instead of \" !");
+        System.out.println("\" is not the same as \"");
+        System.out.println();
+
+        //Question 4.
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+        System.out.println();
+
+        //Question 5.
+        System.out.println("*********");
+        System.out.println("*********");
+        System.out.println("*********");
+        System.out.println("*********");
+        System.out.println("*********");
+        System.out.println();
+
+        //Question 6.
+        System.out.println("A well-formed Java program has a main method with { and } braces.");
+        System.out.println();
+        System.out.println("A System.out.println statement has ( and ) and usually a String that starts and ends with a \" character.");  
+        System.out.println("(But we type\\\" instaead!)\nWhat is the difference between\n a \' and a \"? Or between a \" and a \"?");      
+        System.out.println();
+        System.out.println("One is what we see when we\'re typing our program. The other is what appears on the \"console.\"");
+        System.out.println();
+                
+    }
+}
+
